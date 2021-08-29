1f9987d652838e2c22e0581ee5e3d4926ffe14f8
ChristinaLast <christina.last@outlook.com>
2021-08-29 19:28:05 +0100

    :sparkles: adding functionality to pull building footprints and calculate their centroids using osm.

--- reinventing_catastrophe_modelling/src/generate_building_centroids.py
+++ reinventing_catastrophe_modelling/src/generate_building_centroids.py
@@ -2,14 +2,16 @@ from typing import Dict, Sequence, Any
 from joblib import Parallel, delayed
 
 import osmnx as ox
+import pandas as pd
 import geopandas as gpd
 
 from config.model_settings import DataConfig, OSMConfig
 
 
 class GenerateBuildingCentroids:
-    def __init__(self, countries: Sequence, tags: Dict[str, Any]):
+    def __init__(self, countries: Sequence, place: str, tags: Dict[str, Any]):
         self.countries = countries
+        self.place = place
         self.tags = tags
 
     @classmethod
@@ -21,20 +23,23 @@ class GenerateBuildingCentroids:
             country_info = data_config.COUNTRY_BOUNDING_BOXES.get(country, "WO")
             countries.append(country_info)
 
-        return cls(countries=countries, tags=osm_config.TAG)
+        return cls(countries=countries, place=osm_config.PLACE, tags=osm_config.TAGS)
 
     def execute(self):
-        Parallel(n_jobs=-1, backend="multiprocessing", verbose=5)(
-            delayed(self.execute_for_country)(country) for country in self.countries
-        )
+        building_footprint_gdfs = Parallel(
+            n_jobs=-1, backend="multiprocessing", verbose=5
+        )(delayed(self.execute_for_country)(country) for country in self.countries)
+        return pd.concat(building_footprint_gdfs)
 
     def execute_for_country(self, country: str):
-        self.get_representative_building_point
+        return self.get_representative_building_point()
 
     def _get_boundaries_from_place(self) -> gpd.GeoDataFrame:
-        return ox.geometries.geometries_from_place(self.country[0], self.tags)
+        return ox.geometries.geometries_from_place(self.place, self.tags)
 
     def get_representative_building_point(self) -> gpd.GeoDataFrame:
         building_footprints = self._get_boundaries_from_place()
-        building_footprints.centroid_geometry = building_footprints.representative_point()
+        building_footprints[
+            "centroid_geometry"
+        ] = building_footprints.representative_point()
         return building_footprints
