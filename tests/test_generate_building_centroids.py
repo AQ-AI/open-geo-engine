67124a2e5ae6aa657fce96a7d9e8ce01788f4077
ChristinaLast <christina.last@outlook.com>
2022-01-16 00:23:50 +0000

    :white_check_mark: adding passing tests

---
+++ tests/test_generate_building_centroids.py
@@ -0,0 +1,14 @@
+from open_geo_engine.src.generate_building_centroids import GenerateBuildingCentroids
+
+
+def test_get_boundaries_from_place():
+    countries = ["ES"]
+    place = "Parque El Retiro, Madrid"
+    tags = {"leisure": "park"}
+
+    generate_building_centroids = GenerateBuildingCentroids(countries, place, tags)
+    assert generate_building_centroids._get_boundaries_from_place().shape == (1, 20)
+    assert generate_building_centroids.get_representative_building_point().shape == (
+        1,
+        21,
+    )
