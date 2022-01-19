65843d3cf6ecb980c4176d39a70dac6c0dfd6f89
ChristinaLast <christina.last@outlook.com>
2022-01-19 08:12:47 +0000

    :wrench: updating paths in model settings

--- open_geo_engine/config/model_settings.py
+++ open_geo_engine/config/model_settings.py
@@ -14,13 +14,13 @@ class StreetViewConfig:
     PITCH: str = "-0.76"
     KEY = os.environ.get("GOOGLE_DEV_API_KEY")
     LOCAL_IMAGE_FOLDER: str = (
-        f"{Path(__file__).resolve().parent.parent.parent}/local_data/streetview_images"
+        f"{Path(__file__).resolve().parent.parent.parent.parent.parent.parent.parent.parent}/local_data/streetview_images"
     )
     LOCAL_LINKS_FOLDER: str = (
-        f"{Path(__file__).resolve().parent.parent.parent}/local_data/streetview_links"
+        f"{Path(__file__).resolve().parent.parent.parent.parent.parent.parent.parent.parent}/local_data/streetview_links"
     )
     LOCAL_METADATA_FOLDER: str = (
-        f"{Path(__file__).resolve().parent.parent.parent}/local_data/streetview_metadata"
+        f"{Path(__file__).resolve().parent.parent.parent.parent.parent.parent.parent.parent}/local_data/streetview_metadata"
     )
     PLACE = "Santo_Domingo_Dominican_Republic"
     META_BASE = "https://maps.googleapis.com/maps/api/streetview/metadata?"
