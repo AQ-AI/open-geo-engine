d3151172eeca935f539036b6f8cef61711a38084
ChristinaLast <christina.last@outlook.com>
2022-08-05 00:16:45 -0400

    :sparkles: enabling loadeedata to work

--- open_geo_engine/__main__.py
+++ open_geo_engine/__main__.py
@@ -1,7 +1,11 @@
 import click
 import ee
 
-from open_geo_engine.config.model_settings import DataConfig, OSMConfig, StreetViewConfig
+from open_geo_engine.config.model_settings import (
+    DataConfig,
+    OSMConfig,
+    StreetViewConfig,
+)
 from open_geo_engine.src.generate_building_centroids import GenerateBuildingCentroids
 from open_geo_engine.src.get_google_streetview import GetGoogleStreetView
 from open_geo_engine.src.load_ee_data import LoadEEData
@@ -20,35 +24,36 @@ class GenerateBuildingCentroidsFlow:
 
 
 class LoadDataFlow:
-    def __init__(self, filepath):
+    def __init__(self):
         self.config = DataConfig()
-        self.filepath = filepath
 
-    def execute(self):
+    def execute(self, building_footprint_gdf):
         # Trigger the authentication flow.
         ee.Authenticate()
         data_loader = LoadEEData.from_dataclass_config(self.config)
 
-        data_loader.execute(self.filepath, save_images=True)
+        data_loader.execute(building_footprint_gdf, save_images=False)
 
-    def execute_for_country(self, building_footprint_gdf):
+    def execute_for_country(self):
         # Trigger the authentication flow.
         ee.Authenticate()
         data_loader = LoadEEData.from_dataclass_config(self.config)
 
-        return data_loader.execute_for_country(building_footprint_gdf, save_images=True)
+        return data_loader.execute_for_country(save_images=False)
 
 
 class GetGoogleStreetViewFlow:
     def __init__(self):
         self.streetview_config = StreetViewConfig()
 
-    def execute_for_country(self, satellite_data_df):
+    def execute_for_df(self, satellite_data_df):
         # Trigger the authentication flow.
         ee.Authenticate()
-        streetview_downloader = GetGoogleStreetView.from_dataclass_config(self.streetview_config)
+        streetview_downloader = GetGoogleStreetView.from_dataclass_config(
+            self.streetview_config
+        )
 
-        return streetview_downloader.execute_for_country(satellite_data_df)
+        return streetview_downloader.execute_for_df(satellite_data_df)
 
 
 @click.command("generate_building_centroids", help="Retrieve building centroids")
@@ -58,20 +63,25 @@ def generate_building_centroids():
 
 @click.command("load_data", help="Load data from Google Earth Engine")
 def load_data():
-
     LoadDataFlow().execute()
 
 
-@click.command("get_google_streetview", help="Retrieve streetview images for building locations")
+@click.command(
+    "get_google_streetview", help="Retrieve streetview images for building locations"
+)
 def get_google_streetview(satellite_data_df):
     GetGoogleStreetViewFlow().execute_for_country(satellite_data_df)
 
 
 @click.command("run_pipeline", help="Run full analysis pipeline")
-def run_full_pipeline():
+@click.argument("path_to_local_data")
+def run_full_pipeline(path_to_local_data):
     building_footprint_gdf = GenerateBuildingCentroidsFlow().execute()
-    satellite_data_df = LoadDataFlow().execute_for_country(building_footprint_gdf)
-    GetGoogleStreetViewFlow().execute_for_country(satellite_data_df)
+    building_footprint_gdf.to_csv(
+        f"{path_to_local_data}/osm_data/building_footprint.csv"
+    )
+    satellite_data_df = LoadDataFlow().execute(building_footprint_gdf)
+    # GetGoogleStreetViewFlow().execute_for_df(satellite_data_df)
 
 
 @click.group(
