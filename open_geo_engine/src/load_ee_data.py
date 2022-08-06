eff7587e4eefd53faad7e2bf81d9705a6854e242
ChristinaLast <christina.last@outlook.com>
2022-08-06 23:22:39 +0100

    updating load_ee_data

--- open_geo_engine/src/load_ee_data.py
+++ open_geo_engine/src/load_ee_data.py
@@ -1,6 +1,6 @@
 import datetime
 from typing import Any, Sequence, Tuple
-
+import logging
 import ee
 import geemap
 import pandas as pd
@@ -59,62 +59,75 @@ class LoadEEData:
             year_end=config.YEAR_END,
             mon_end=config.MON_END,
             date_end=config.DATE_END,
-            image_collection=config.LANDSAT_IMAGE_COLLECTION,
-            image_band=config.LANDSAT_IMAGE_BAND,
+            image_collection=config.AOD_IMAGE_COLLECTION,
+            image_band=config.AOD_IMAGE_BAND,
             folder=config.BASE_FOLDER,
             image_folder=config.IMAGE_FOLDER,
             model_name=config.MODEL_NAME,
             place=config.PLACE,
         )
 
-    def execute(self, filepath, save_images):
-        building_footprint_gdf = pd.read_csv(filepath)
-
-        Parallel(n_jobs=-1, backend="multiprocessing", verbose=5)(
-            delayed(self.execute_for_country)(building_footprint_gdf, save_images)
+    def execute(self, building_footprint_gdf, save_images, **kwargs):
+        if kwargs.get("filepath"):
+            building_footprint_gdf = pd.read_csv(kwargs.get("filepath"))
+        building_footprints_satellite_df = Parallel(
+            n_jobs=-1, backend="multiprocessing", verbose=5
+        )(
+            delayed(self.execute_for_country)(
+                building_footprint_gdf, country, save_images
+            )
             for country in self.countries
         )
+        return building_footprints_satellite_df
 
-    def execute_for_country(self, building_footprint_gdf, save_images):
-        print(f"Downloading {self.model_name} data for {self.place}")
+    def execute_for_country(self, building_footprint_gdf, country, save_images):
+        logging.info(f"Downloading {self.model_name} data for {self.place}")
+        ee.Initialize()
+        coords_tup = country[1]
+        s_datetime, e_datetime = self._generate_start_end_date()
+        geom = ee.Algorithms.GeometryConstructors.BBox(
+            coords_tup[0], coords_tup[1], coords_tup[2], coords_tup[3]
+        )
+        collection = (
+            ee.ImageCollection(self.image_collection)
+            .filterBounds(geom)
+            .select(self.image_band)
+            .filterDate(s_datetime, e_datetime)
+        )
+        if save_images is True:
+            self.save_images_to_drive(collection, s_datetime, e_datetime, country)
+
         building_footprint_gdf = self._get_xy(building_footprint_gdf)
-        building_footprints_satellite_list = []
+        osm_ee_list = []
         for lon, lat in zip(building_footprint_gdf.x, building_footprint_gdf.y):
-            # Initialize the library.
-            ee.Initialize()
             centroid_point = ee.Geometry.Point(lon, lat)
-            s_datetime, e_datetime = self._generate_start_end_date()
-            collection = (
-                ee.ImageCollection(self.image_collection)
-                .filterBounds(centroid_point)
-                .select(self.image_band)
-                .filterDate(s_datetime, e_datetime)
-            )
             landsat_centroid_point = self._get_centroid_value_from_collection(
                 collection, centroid_point
             )
-            building_footprints_satellite_list.append(
-                ee_array_to_df(landsat_centroid_point, self.image_band)
-            )
-            if save_images is True:
-                self.save_images_to_drive(collection, s_datetime, e_datetime, lon, lat)
-        return pd.concat(building_footprints_satellite_list)
 
-    def save_images_to_drive(self, collection, s_datetime, e_datetime, lon, lat):
+            ee_df = ee_array_to_df(landsat_centroid_point, self.image_band)
+            if not ee_df.empty:
+                osm_ee_list.append(ee_df)
+        if len(self.countries) == 1:
+            osm_ee_df = pd.concat(osm_ee_list)
+            osm_ee_df.to_csv(f"local_data/gee_data/{country[0]}_{self.model_name}.csv")
+            return osm_ee_df
+        else:
+            return pd.concat(osm_ee_list)
+
+    def save_images_to_drive(self, collection, s_datetime, e_datetime, country):
         s_date = s_datetime.date()
         e_date = e_datetime.date()
-        lon_without_symbol = self._replace_symbol(lon)
-        lat_without_symbol = self._replace_symbol(lat)
         geemap.ee_export_image_collection(
             collection,
-            out_dir=f"{self.image_folder}/{self.model_name}_{s_date}_{e_date}_{lat_without_symbol}_{lon_without_symbol}",
+            out_dir=f"{self.image_folder}/{self.model_name}_{s_date}_{e_date}_{country[0]}",
         )
 
     def _get_centroid_value_from_collection(self, collection, centroid_point):
         try:
             return collection.getRegion(centroid_point, 10).getInfo()
         except (EEException, HttpError):
-            print(centroid_point)
+            logging.error(f" Invalid centroid {centroid_point}")
             pass
 
     def _generate_start_end_date(self) -> Tuple[datetime.date, datetime.date]:
@@ -138,8 +151,12 @@ class LoadEEData:
         except TypeError:
             pass
 
-        building_footprint_gdf["x"] = building_footprint_gdf.centroid_geometry.map(lambda p: p.x)
-        building_footprint_gdf["y"] = building_footprint_gdf.centroid_geometry.map(lambda p: p.y)
+        building_footprint_gdf["x"] = building_footprint_gdf.centroid_geometry.map(
+            lambda p: p.x
+        )
+        building_footprint_gdf["y"] = building_footprint_gdf.centroid_geometry.map(
+            lambda p: p.y
+        )
         return building_footprint_gdf
 
     def _replace_symbol(self, item):
